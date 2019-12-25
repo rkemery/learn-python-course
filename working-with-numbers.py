8a86d4a15af38a277c9b0916e031b705ebd57947
Rick Kemery <rick.kemery@gmail.com>
2019-12-25 12:53:44 -0500

    first commit

---
+++ working-with-numbers.py
@@ -0,0 +1,24 @@
+from math import  *
+
+print(-2.0987)
+print(3 + 4)
+print(3 + 4.5)
+print(3 * 4.5)
+print(3 * (4 + 5))
+print(10 % 3)
+print(9 % 3)
+my_num = 5
+print(my_num)
+print(str(my_num))
+print(str(my_num) + " my favorite number")
+my_num = -6
+print(abs(my_num))
+print(pow(3, 2))
+print(pow(4, 6))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.2))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
